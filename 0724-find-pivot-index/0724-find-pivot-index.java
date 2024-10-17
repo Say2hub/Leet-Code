50a1cde884ec63e1daf5ffed0a39c1d2f522fe3f
Sayantan_07 <135653458+Say2hub@users.noreply.github.com>
2024-10-17 16:42:21 +0530

    Time: 1 ms (94.91%), Space: 45.4 MB (57.4%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,8 +1,7 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int n = nums.length;
-        int left = 0;
-        int right = n-1;
+        //Longer Time Complexity but works!
+        /**int n = nums.length;
         int left_sum = 0;
         int right_sum = 0;
         for(int i=0;i<n;i++){
@@ -22,6 +21,20 @@ class Solution {
             right_sum=0;
         }
         }
-return -1;   
+return -1;**/
+//Optimized Version
+int right_sum = 0;
+for(int i:nums){
+    right_sum+=i;
+}
+int left_sum = 0;
+for(int j=0;j<nums.length;j++){
+    right_sum -= nums[j];
+    if(left_sum==right_sum){
+        return j;
+    }
+    left_sum+=nums[j];
+}
+return -1;
     }
 }
