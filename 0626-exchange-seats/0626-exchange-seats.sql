f47253d7038d3ffd4a8c072342035c505183fa5b
Sayantan_07 <135653458+Say2hub@users.noreply.github.com>
2025-02-15 15:48:17 +0530

    Time: 847 ms (15.71%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,7 +1,7 @@
 WITH cte AS(
 SELECT * , 
-LEAD(id) OVER(ORDER BY id) AS next,
-LAG(id) OVER(ORDER BY id) AS prev
+LEAD(id) OVER(ORDER BY id) AS next, #Lead,Lag are Window Functions
+LAG(id) OVER(ORDER BY id) AS prev   #Lead keeps value of next Running ID, Lag Keeps Previous Id.#Over Indicates that it is a Window Function
 FROM Seat
 )
 SELECT  CASE 
