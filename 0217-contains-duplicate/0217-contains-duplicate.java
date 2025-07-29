52cd2f2f945dc5766d855470c25242e83cc757e5
Sayantan Mukherjee <135653458+Say2hub@users.noreply.github.com>
2025-07-29 14:05:33 +0530

    Time: 19 ms (36.48%), Space: 58.3 MB (61.95%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,16 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
-        int count = 1;
         int i = 0;
         while(i < nums.length-1){
             if(nums[i]==nums[i+1]){
-                count++;
-            }
-            if(count>1){
                 return true;
             }
-            i++;
+            i++;  
         }
     return false;
     }
