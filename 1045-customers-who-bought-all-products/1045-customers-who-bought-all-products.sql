2d5e5edac0d9a1f9a50faeb6a43cc3b7f7b573cc
Sayantan Mukherjee <135653458+Say2hub@users.noreply.github.com>
2025-08-30 13:20:54 +0530

    Time: 588 ms (42.97%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -2,6 +2,6 @@
 SELECT customer_id
 FROM Customer
 GROUP BY customer_id
-HAVING COUNT(DISTINCT(product_key))=(SELECT COUNT(DISTINCT(product_key))FROM Product)
+HAVING COUNT(DISTINCT(product_key))=(SELECT COUNT(product_key)FROM Product)
 
 
